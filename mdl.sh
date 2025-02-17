840abf8882498acd8546d2a3057561fc2b027f51
Jacob Rambarran <jacobrambarran@gmail.com>
2025-02-17 01:41:46 -0500

    removed comments

--- mdl.sh
+++ mdl.sh
@@ -15,24 +15,20 @@
 #list search urls 
 # curl -s "https://manganato.com/search/story/naruto" | htmlq -a href 'div.search-story-item a.item-img'
 
-MANGA_ID="aa951409"  # Change this to the actual manga ID
-CHAPTER_NUM="1"       # Change this for the chapter number
+MANGA_ID="aa951409"  
+CHAPTER_NUM="1"     
 CHAPTER_URL="https://chapmanganato.to/manga-${MANGA_ID}/chapter-${CHAPTER_NUM}"
 OUTPUT_DIR="One-Piece"
 CBZ_FILENAME="${OUTPUT_DIR}/Ch.${CHAPTER_NUM}.cbz"
 
-# Create output directory if it doesn’t exist
 mkdir -p "$OUTPUT_DIR"
 
 echo "Fetching images from: $CHAPTER_URL"
 
-# Get image URLs from the chapter page
 image_urls=$(curl -s -A "Mozilla/5.0" "$CHAPTER_URL" | htmlq -a src 'div.container-chapter-reader img')
 
 printf "%s\n" "$image_urls"
-# echo "Found $(echo "$image_urls" | wc -l) images. Downloading..."
 
-# Download images and store in CBZ format (ZIP)
 zip_file="${CBZ_FILENAME}.tmp.zip"
 rm -f "$zip_file"
 
@@ -44,16 +40,13 @@ while IFS= read -r img_url; do
     
     curl -s -A "Mozilla/5.0" -e "$CHAPTER_URL" -o "$OUTPUT_DIR/$img_name" "$img_url"
     
-    # Add to ZIP
     zip -jq "$zip_file" "$OUTPUT_DIR/$img_name"
     
-    # Remove downloaded file after adding to ZIP
     rm -f "$OUTPUT_DIR/$img_name"
 
     ((index++))
 done <<< "$image_urls"
 
-# Rename ZIP to CBZ
 mv "$zip_file" "$CBZ_FILENAME"
 
 echo "Saved: $CBZ_FILENAME"
